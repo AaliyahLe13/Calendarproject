13010630c6f51df2ff203701d8b2985ccdc32c62
evelynvhall <108427310+evelynvhall@users.noreply.github.com>
2024-11-09 10:44:46 -0600

    calender.py

--- calender.py
+++ calender.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    session['email'] = request.form['email_address']
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        # Check username and password
+        # If valid
+        session["username"] = "username" # Replace with real username
+        return render_template("index.html")
+        # If invalid
+        return render_template("login.html", warning = "Invalid username or password")
