2f3ab23f5a71bcf432dee49abd7c39588f1ca89e
evelynvhall <108427310+evelynvhall@users.noreply.github.com>
2024-11-09 11:10:26 -0600

    Update calender.py

--- calender.py
+++ calender.py
@@ -7,7 +7,6 @@ def index():
     # Some sort of thing that sends you to the login route if you 
     if not session["username"]:
         #Something that sends you to the login page
-        
     if request.method == "GET":
         # Something that gets information from database and sends it to the calendar
         return render_template("todo.html")
@@ -23,6 +22,20 @@ def login():
     if request.method == "POST":
         # Check username and password
         # If valid
+        session["username"] = "username" # Replace with real username
+        return render_template("index.html")
+        # If invalid
+        return render_template("login.html", warning = "Invalid username or password")
+
+@app.route("/new-user", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("new-user.html")
+    if request.method == "POST":
+        # Check username doesn't already exist
+        # Check password is long enough
+        # Check passoword and conformation are right
+        # Email confirmation send
         session["username"] = "username" # Replace with real username
         return render_template("index.html")
         # If invalid
